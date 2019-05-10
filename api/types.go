df8dc2dceea408735bb5ef87361fc2f10ef215a3
Vinod <jvinod@portworx.com>
2019-05-10 05:56:33 +0000

    Add name, region. Change to EncryptionID. Make comments idiomatic Golang
    Signed-off-by: Vinod <jvinod@portworx.com>

--- api/types.go
+++ api/types.go
@@ -6,23 +6,26 @@ import (
 
 // CloudVolumeSpec defines volume spec.
 type CloudVolumeSpec struct {
-	// VolumeType the volume type (e.g. GP2 on AWS)
-	VolumeType *string `type:"string" enum:"VolumeType"`
+	// Name volume name
+	Name *string `locationName:"name" type:"string" required:"true"`
+	// Type the volume type (e.g. GP2 on AWS)
+	Type *string `locationName:"type" type:"string" required:"true"`
 	// Size in GiBs.
-	Size *int64 `type:"integer"`
-	// Availability zone in which to create the volume.
-	AvailabilityZone *string `type:"string" required:"true"`
-	// IOPS desired IOPS. Optional.
-	Iops *int64 `type:"integer"`
-	// Encrypted specifies whether the volume should be encrypted. Optional
-	Encrypted *bool `locationName:"encrypted" type:"boolean"`
-	// KmsKeyID identifier for the Key Management Service and specifies customer master
-	// key (CMK) to use when creating the encrypted volume.  Optional
-	KmsKeyID *string `type:"string"`
+	Size *int64 `locationName:"size" type:"string" required:"true"`
+	// Region in which to create the volume.
+	Region *string `locationName:"region" type:"string" required:"false"`
+	// Zone in which to create the volume.
+	Zone *string `locationName:"zone" type:"string" required:"true"`
+	// Iops desired IOPS.
+	Iops *int64 `type:"integer" required:"false"`
+	// Encrypted specifies whether the volume should be encrypted.
+	Encrypted *bool `locationName:"encrypted" type:"boolean" required:"false"`
+	// EncryptionKeyID identifier for the Key Management Service
+	EncryptionKeyID *string `locationName:"encryption_key_id" type:"string" required:"false"`
 	// SnapshotID the snapshot from which to create the volume. Optional
-	SnapshotID *string `type:"string"`
+	SnapshotID *string `locationName:"snapshot_id" type:"string" required:"false"`
 	// The tags to apply to the volume during creation. Optional
-	Labels map[string]string `locationName:"Labels"`
+	Labels map[string]string `locationName:"Labels" required:"false"`
 }
 
 // VolumeAttachmentState enum for current volume attachment state.
@@ -47,9 +50,9 @@ type CloudVolumeAttachment struct {
 	DeviceName *string `locationName:"device" type:"string"`
 	// InstanceID unique instance identifier.
 	InstanceID *string `locationName:"instanceID" type:"string"`
-	// The attachment state of the volume.
+	// State the attachment state of the volume.
 	State *string `locationName:"status" type:"string" enum:"VolumeAttachmentState"`
-	// The ID of the volume.
+	// VolumeID unique identifier for volume.
 	VolumeID *string `locationName:"volumeID" type:"string"`
 }
 
@@ -59,17 +62,18 @@ type CloudVolume struct {
 	VolumeID *string `locationName:"volumeID" type:"string"`
 	// Attachement information
 	Attachment *CloudVolumeAttachment `locationName:"attachment"`
-	// AvailabilityZone for the volume.
-	AvailabilityZone *string `locationName:"availabilityZone" type:"string"`
+	// Zone for the volume.
+	Zone *string `locationName:"zone" type:"string"`
+	// Region for the volume.
+	Region *string `locationName:"region" type:"string"`
 	// CreateTime the time stamp when volume creation was initiated.
 	CreateTime *time.Time `locationName:"createTime" type:"timestamp"`
 	// Encrypted indicates whether the volume will be encrypted.
 	Encrypted *bool `locationName:"encrypted" type:"boolean"`
 	// Iops provisioned for this volume.
 	Iops *int64 `locationName:"iops" type:"integer"`
-	// KmsKeyID the full ARN of the Key Management Service, customer master
-	// key (CMK) that was used to protect the volume encryption key for the volume.
-	KmsKeyID *string `locationName:"kmsKeyID" type:"string"`
+	// EncryptionKeyID the full ARN of the Key Management Service.
+	EncryptionKeyID *string `locationName:"kmsKeyID" type:"string"`
 	// Size in GiBs.
 	Size *int64 `locationName:"size" type:"integer"`
 	// SnapshotID from which the volume was created, if applicable.
@@ -80,5 +84,4 @@ type CloudVolume struct {
 	Labels map[string]string `locationName:"labels" locationNameList:"item" type:"list"`
 	// VolumeType the type of the volume e.g. GP2j
 	VolumeType *string `locationName:"volumeType" type:"string" enum:"VolumeType"`
-	// contains filtered or unexported fields
 }
