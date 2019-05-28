3ec3dc987128e5024417509462a97537d49ab02a
Vinayak Shinde <vinayakshnd@gmail.com>
2019-05-28 18:16:02 +0530

    * Added SetInstanceGroupSize() and GetClusterSizeForInstance() API implementation in backoff/exponential

--- backoff/exponential.go
+++ backoff/exponential.go
@@ -89,6 +89,43 @@ func (e *exponentialBackoff) InspectInstanceGroupForInstance(instanceID string) 
 	return instanceGroupInfo, origErr
 }
 
+func (e *exponentialBackoff) SetInstanceGroupSize(instanceGroupID string, clusterLocation string,
+	count int64,
+	timeout time.Duration) error {
+	var (
+		origErr error
+	)
+	conditionFn := func() (bool, error) {
+		origErr = e.cloudOps.SetInstanceGroupSize(instanceGroupID, clusterLocation, count, timeout)
+		msg := fmt.Sprintf("Failed to set instance group size for: %v.", instanceGroupID)
+		return e.handleError(origErr, msg)
+	}
+	expErr := wait.ExponentialBackoff(e.backoff, conditionFn)
+	if expErr == wait.ErrWaitTimeout {
+		return cloudops.NewStorageError(cloudops.ErrExponentialTimeout, origErr.Error(), "")
+	}
+	return origErr
+
+}
+
+func (e *exponentialBackoff) GetClusterSizeForInstance(instanceID string) (int64, error) {
+	var (
+		count   int64
+		origErr error
+	)
+	conditionFn := func() (bool, error) {
+		count, origErr = e.cloudOps.GetClusterSizeForInstance(instanceID)
+		msg := fmt.Sprintf("Failed to get instance group size for: %v.", instanceID)
+		return e.handleError(origErr, msg)
+	}
+	expErr := wait.ExponentialBackoff(e.backoff, conditionFn)
+	if expErr == wait.ErrWaitTimeout {
+		return 0, cloudops.NewStorageError(cloudops.ErrExponentialTimeout, origErr.Error(), "")
+	}
+	return count, origErr
+
+}
+
 // Create volume based on input template volume and also apply given labels.
 func (e *exponentialBackoff) Create(template interface{}, labels map[string]string) (interface{}, error) {
 	var (
