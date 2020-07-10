4cc517d4ed388d4dd090b48cedfa9df8dc99d3a0
Harsh Desai <harsh@portworx.com>
2020-07-10 10:32:45 -0700

    For first class disks, use disk type provided by the user (#65)
    Signed-off-by: Harsh Desai <harsh@portworx.com>

--- vsphere/lib/vsphere/vclib/constants.go
+++ vsphere/lib/vsphere/vclib/constants.go
@@ -22,6 +22,7 @@ const (
 	PreallocatedDiskType     = "preallocated"
 	EagerZeroedThickDiskType = "eagerZeroedThick"
 	ZeroedThickDiskType      = "zeroedThick"
+	LazyZeroedThickDiskType  = "lazyZeroedThick"
 )
 
 // Controller Constants
