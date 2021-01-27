500c76ca57ba7b98d99c4b434300abf25a357550
Harsh Desai <harsh@portworx.com>
2021-01-27 12:10:42 -0800

    Add InstanceState in inspect info return value. Add support for GCE. (#72)
    * add instance state for GCE
    
    Signed-off-by: Harsh Desai <harsh@portworx.com>
    
    * go lint fix
    
    Signed-off-by: Harsh Desai <hadesai@purestorage.com>

--- gce/gce_test.go
+++ gce/gce_test.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"testing"
 
+	"github.com/sirupsen/logrus"
+
 	"github.com/libopenstorage/cloudops"
 	"github.com/libopenstorage/cloudops/gce"
 	"github.com/libopenstorage/cloudops/test"
@@ -50,6 +52,20 @@ func TestAll(t *testing.T) {
 		fmt.Printf("skipping GCE tests as environment is not set...\n")
 		t.Skip("skipping GCE tests as environment is not set...")
 	}
+}
+
+func TestInspectInstance(t *testing.T) {
+	if gce.IsDevMode() {
+		d, _ := initGCE(t)
+		info, err := d.InspectInstance(os.Getenv("GCE_INSTANCE_NAME"))
+		require.NoError(t, err)
+		require.NotNil(t, info)
+		logrus.Infof("[debug] got instance info: %v", info)
+	} else {
+		fmt.Printf("skipping GCE tests as environment is not set...\n")
+		t.Skip("skipping GCE tests as environment is not set...")
+	}
+
 }
 
 func sizeCheck(template interface{}, targetSize uint64) bool {
