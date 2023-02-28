56c719875e8cc988483238052fc707a609eaaaab
alice-px <alyu@purestorage.com>
2023-02-28 10:57:27 -0800

    update

--- azure/azure.go
+++ azure/azure.go
@@ -604,20 +604,22 @@ func (a *azureOps) SupportOnlineResize(diskName string, newSizeGB uint64) string
 	if disk.DiskProperties.ShareInfo != nil {
 		return "shared disk does not support online resize"
 	}
-	// disk type can't be Ultra disks or Premium SSD v2: this type is not available in the sdk version we use
-	// https:// github.com/hashicorp/go-azure-sdk/blob/8c5aed068a057da463327aba286338af98a4358e/resource-manager/compute/2022-03-02/disks/constants.go#L231
-	// premium v2 is also only available in US east and West Europe
-	// https://learn.microsoft.com/en-us/azure/virtual-machines/disks-deploy-premium-v2?tabs=azure-cli#regional-availability
-	// if disk.Sku.Name == compute.DiskStorageAccountTypesPremiumVTwoLRS {
-	// 	return "disk type premium ssd v2 does not support online resize"
-	// }
 
-	if disk.Sku.Name == "UltraSSD_LRS" {
+	// disk type can't be Ultra disks or Premium SSD v2
+	// premium ssd v2 type is not currently available in the sdk version we use
+	// https://github.com/hashicorp/go-azure-sdk/blob/8c5aed068a057da463327aba286338af98a4358e/resource-manager/compute/2022-03-02/disks/constants.go#L231
+	// premium v2 is only available in US east and West Europe, for which reason portworx does not support it at the moment @piyush
+	// https://learn.microsoft.com/en-us/azure/virtual-machines/disks-deploy-premium-v2?tabs=azure-cli#regional-availability
+	if disk.Sku.Name == "PremiumV2_LRS" {
+		return "disk type premium ssd v2 does not support online resize"
+	}
+
+	if disk.Sku.Name == compute.DiskStorageAccountTypesUltraSSDLRS {
 		return "disk type Ultra SSD type does not support online resize"
 	}
 	// if target size > 4GB, original size can't be less than 4GB
 	if newSizeGB > 4*1024 && *disk.DiskSizeGB <= 4*1024 {
-		return "online resize is not possible when expanding to over 4GB from less than 4GB"
+		return "online resize is not possible when expanding to over 4TiB from less than 4TiB"
 	}
 
 	return ""
