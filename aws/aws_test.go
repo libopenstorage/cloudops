db265b7e4ca1779f8b03fb12867dae2aec39474a
Aditya Dani <aditya@portworx.com>
2023-07-27 19:12:28 +0000

    PWX-28191: Add support for reading AWS credentials from a k8s secret.
    - Use the AWS creds provided through a kubernetes secrets through the
      StaticCredentials provider.
    - Added UTs using a fake k8s clientset. Also run the whole cloudops suite
      using such a static credential provider.
    
    Signed-off-by: Aditya Dani <aditya@portworx.com>

--- aws/aws_test.go
+++ aws/aws_test.go
@@ -2,6 +2,7 @@ package aws
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/service/ec2"
@@ -10,7 +11,12 @@ import (
 	"github.com/libopenstorage/cloudops"
 	"github.com/libopenstorage/cloudops/test"
 	"github.com/pborman/uuid"
+	"github.com/portworx/sched-ops/k8s/core"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
 )
 
 const (
@@ -24,7 +30,7 @@ func TestAll(t *testing.T) {
 	drivers := make(map[string]cloudops.Ops)
 	diskTemplates := make(map[string]map[string]interface{})
 
-	if d, err := NewClient(); err == nil {
+	if d, err := NewClient("", ""); err == nil {
 		volType := opsworks.VolumeTypeGp2
 		volSize := int64(newDiskSizeInGB)
 		zone, _ := cloudops.GetEnvValueStrict("AWS_ZONE")
@@ -138,7 +144,111 @@ func TestAwsCreate(t *testing.T) {
 			},
 		}
 		res, err := s.Create(c.volumeTemplate, nil, nil)
-        assert.Equal(t, err.Error(), c.expectedErr.Error(), "%s failed, expected error: \n%v\n but got: \n%v", c.name, c.expectedErr, err)
-        assert.Equal(t, res, c.volResult, "%s failed, expected result %s but got %s", c.name, c.volResult, res)
+		assert.Equal(t, err.Error(), c.expectedErr.Error(), "%s failed, expected error: \n%v\n but got: \n%v", c.name, c.expectedErr, err)
+		assert.Equal(t, res, c.volResult, "%s failed, expected result %s but got %s", c.name, c.volResult, res)
 	}
+}
+
+func TestAllWithKubernetes(t *testing.T) {
+
+	// Create a new fake clientset
+	client := fake.NewSimpleClientset()
+	schedClient := core.New(client)
+	core.SetInstance(schedClient)
+
+	k8sSecretName := "px-aws"
+	k8sSecretNamespace := "portworx"
+
+	// Test Case: Valid AWS credentials
+
+	// Fetch the caller provided credentials from the env variables
+	// and put them into a kubernetes secret.
+	_, err := core.Instance().CreateSecret(&corev1.Secret{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      k8sSecretName,
+			Namespace: k8sSecretNamespace,
+		},
+		Data: map[string][]byte{
+			awsAccessKeyName:       []byte(os.Getenv(awsAccessKeyName)),
+			awsSecretAccessKeyName: []byte(os.Getenv(awsSecretAccessKeyName)),
+		},
+	})
+	require.NoError(t, err, "failed to create fake secret")
+
+	// Unset the aws credentials from the environment variables
+	// so that the static credentials from the k8s secret are used.
+	os.Unsetenv(awsAccessKeyName)
+	os.Unsetenv(awsSecretAccessKeyName)
+
+	drivers := make(map[string]cloudops.Ops)
+	diskTemplates := make(map[string]map[string]interface{})
+
+	if d, err := NewClient(k8sSecretName, k8sSecretNamespace); err == nil {
+		volType := opsworks.VolumeTypeGp2
+		volSize := int64(newDiskSizeInGB)
+		zone, _ := cloudops.GetEnvValueStrict("AWS_ZONE")
+		ebsVol := &ec2.Volume{
+			AvailabilityZone: &zone,
+			VolumeType:       &volType,
+			Size:             &volSize,
+		}
+		drivers[d.Name()] = d
+		diskTemplates[d.Name()] = map[string]interface{}{
+			diskName: ebsVol,
+		}
+	} else {
+		t.Skipf("skipping AWS tests as environment is not set...\n")
+	}
+
+	test.RunTest(drivers, diskTemplates, sizeCheck, t)
+
+	// Test Case: Missing aws access key
+	_, err = core.Instance().UpdateSecret(&corev1.Secret{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      k8sSecretName,
+			Namespace: k8sSecretNamespace,
+		},
+	})
+	require.NoError(t, err, "failed to create fake secret")
+
+	c, err := NewClient(k8sSecretName, k8sSecretNamespace)
+	require.Contains(t, err.Error(), fmt.Sprintf("%v not found in k8s secret", awsAccessKeyName))
+	require.Nil(t, c)
+
+	// Test Case: Missing aws secret key
+	_, err = core.Instance().UpdateSecret(&corev1.Secret{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      k8sSecretName,
+			Namespace: k8sSecretNamespace,
+		},
+		Data: map[string][]byte{
+			awsAccessKeyName: []byte("access-key"),
+		},
+	})
+	require.NoError(t, err, "failed to create fake secret")
+
+	c, err = NewClient(k8sSecretName, k8sSecretNamespace)
+	require.Contains(t, err.Error(), fmt.Sprintf("%v not found in k8s secret", awsSecretAccessKeyName))
+	require.Nil(t, c)
+
+	// Test Case: Invalid aws credentials
+	_, err = core.Instance().UpdateSecret(&corev1.Secret{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      k8sSecretName,
+			Namespace: k8sSecretNamespace,
+		},
+		Data: map[string][]byte{
+			awsAccessKeyName:       []byte("access-key"),
+			awsSecretAccessKeyName: []byte("secret-key"),
+		},
+	})
+	require.NoError(t, err, "failed to create fake secret")
+
+	c, err = NewClient(k8sSecretName, k8sSecretNamespace)
+	require.NoError(t, err)
+	require.NotNil(t, c)
+
+	vols, err := c.Enumerate(nil, nil, "")
+	require.Error(t, err)
+	require.Empty(t, vols)
 }
